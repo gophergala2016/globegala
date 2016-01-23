e8f5018e2af429a81254f452deb09b90e6ec57e5
thellimist <mufuyil@gmail.com>
2016-01-23 12:39:22 -0800

    Getting port from env

--- main.go
+++ main.go
@@ -4,20 +4,24 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gophergala2016/globegala/Godeps/_workspace/src/github.com/julienschmidt/httprouter"
 	"github.com/gophergala2016/globegala/handlers"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	router := httprouter.New()
 	router.GET("/", handlers.Index)
 	router.GET("/test", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 		fmt.Fprint(w, "TESTTT")
 	})
 
-	port := 5151
-	log.Printf("Server Started @ %d\n", port)
-
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), router))
+	log.Printf("Server Started @ %s\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
