445cbaa911ff7598f283d17b2a11a37e919fa9c4
audreylim <audreylh@gmail.com>
2016-01-23 12:44:05 -0800

    Add fetch contributors and user api

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,19 +2,55 @@ package handlers
 
 import (
 	"fmt"
-	//	"log"
+	"log"
 	"net/http"
 
 	"github.com/gophergala2016/globegala/Godeps/_workspace/src/github.com/julienschmidt/httprouter"
 	"github.com/gophergala2016/globegala/github"
 )
 
+type AllReposData struct {
+	data []RepoData
+}
+
+type RepoData struct {
+	Name         string
+	Contributors []github.Contributor
+}
+
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	repos, err := github.FetchAllRepos()
 	if err != nil {
-		fmt.Println("err", err)
+		log.Fatal("err", err)
 	}
-	fmt.Fprint(w, repos[0].Name)
+
+	var allReposData AllReposData
+	var repoData RepoData
+
+	for _, repo := range repos {
+		contributors, err := github.FetchAllContributors(repo.Name)
+		if err != nil {
+			fmt.Printf("FetchAllContributors: %v", err)
+		}
+
+		if len(contributors) == 0 {
+			continue
+		}
+
+		for _, contributor := range contributors {
+			c, err := github.FetchContributor(contributor.Login)
+			if err != nil {
+				fmt.Printf("FetchContributor: %v", err)
+			}
+
+			repoData.Contributors = append(repoData.Contributors, c)
+		}
+		repoData.Name = repo.Name
+
+		allReposData.data = append(allReposData.data, repoData)
+	}
+
+	fmt.Println(allReposData)
 }
 
 //func Repo(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
