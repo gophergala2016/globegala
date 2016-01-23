d4fe938b7c3bcb84e2d1e3f07f9a7beb680414f3
audreylim <audreylh@gmail.com>
2016-01-23 08:43:51 -0800

    Set up basic routes

--- main.go
+++ main.go
@@ -1,4 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "index")
+}
+
+func Repo(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "hello %s", ps.ByName("repo"))
+}
+
 func main() {
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.GET("/:repo", Repo)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
